bb1d1be08257b57a1b1b49dff52bb03b4694ceeb
ViniciusS4ntos <edsonklibrsouza2015@gmail.com>
2025-11-18 17:52:34 -0300

    corrige a inicialização da lista e ajusta a função Listar para melhor legibilidade

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-# Criacao de lista com 1 elemento senao ela nao existe
-lista = []
+# Biblioteca datetime para marca o horario da internacao
+from datetime import datetime as dt
 
+# Criacao de lista com 1 elemento senao ela nao existe
+lista = [{}]
 
 # Funcao Adicionar
 def Adicionar(nome):
@@ -9,16 +11,25 @@ def Adicionar(nome):
 # Funcao Listar
 def  Listar():
     if lista:
-        for i, nome in enumerate(lista):
-            print(f"[{i}] - {nome}]") 
+        for i, pessoa in enumerate(lista):
+            print(f"[{i}] - {pessoa}]") 
     else:
         print("Lista nao existe!")
 
 
 #Rodar o programa
 while True:
-    nome = input("Digite o nome: ")
-    if nome == "":break
-    Adicionar(nome)
+    print("\na - Adicionar novo paciente\nl - Listar pacientes\ns - sair")
+    opcao = input(str("\nDigite a opcao: ")).lower()
+
+    if opcao == "a":
+        nomePaciente = input(str(("Digite o nome do paciente: ")))
+        Adicionar(nomePaciente)
     
-Listar()
+    elif opcao == "l":
+         Listar()
+
+    elif opcao == "s":break
+
+    else:
+         print("Opcao invalida!")
